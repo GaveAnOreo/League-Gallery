b1f2686d11110eeb0fa7a98c98779c04d4229f54
Benedict S. Orio <benedictsorio51@gmail.com>
2025-11-26 21:56:16 +0800

    Replace repo contents with League Gallery site

--- Programming/PYTHON/PROG 3 QUIZ/quiz1.2/messages.py
+++
@@ -1,7 +0,0 @@
-messages = ["Hello, how are you?", "Have a great day!", "Keep up the good work!"]
-
-def show_messages(msg_list):
-    for msg in msg_list:
-        print(msg)
-
-show_messages(messages)
--- Programming/PYTHON/PROG3/hello_admin.py
+++
@@ -1,8 +0,0 @@
-# HELLO ADMIN
-usernames = ['admin', 'Paul', 'Zakeesha', 'Andrea', 'Dale']
-
-for username in usernames:
-    if username == 'admin':
-        print("Hello Admin, would you like to see a status report?")
-    else:
-        print(f"Hello {username}, thank you for logging in again.")
